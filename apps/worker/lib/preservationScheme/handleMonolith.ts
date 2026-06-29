51585018b67b731d6ad7921167b53466713a770e
daniel31x13 <daniel31x13@gmail.com>
2026-06-29 15:08:13 -0400

    security: harden monolith against DNS rebinding

--- apps/worker/lib/preservationScheme/handleMonolith.ts
+++ apps/worker/lib/preservationScheme/handleMonolith.ts
@@ -3,6 +3,7 @@ import { createFile } from "@linkwarden/filesystem";
 import { prisma } from "@linkwarden/prisma";
 import { Link } from "@linkwarden/prisma/client";
 import sanitizeHtmlForMonolith from "./sanitizeHtmlForMonolith";
+import { getSsrfProxyUrl } from "../ssrfProxy";
 
 export default async function handleMonolith(
   link: Link,
@@ -12,6 +13,11 @@ export default async function handleMonolith(
   if (!link.url) return;
 
   const pageContent = await sanitizeHtmlForMonolith(htmlFromPage, link.url);
+
+  const proxyUrl =
+    process.env.ALLOW_PRIVATE_NETWORK_ACCESS === "true"
+      ? null
+      : await getSsrfProxyUrl();
 
   return new Promise<void>((resolve, reject) => {
     const args = [
@@ -30,6 +36,19 @@ export default async function handleMonolith(
       stdio: ["pipe", "pipe", "inherit"],
       signal,
       killSignal: "SIGKILL",
+      env: proxyUrl
+        ? {
+            ...process.env,
+            HTTP_PROXY: proxyUrl,
+            HTTPS_PROXY: proxyUrl,
+            ALL_PROXY: proxyUrl,
+            http_proxy: proxyUrl,
+            https_proxy: proxyUrl,
+            all_proxy: proxyUrl,
+            NO_PROXY: "",
+            no_proxy: "",
+          }
+        : process.env,
     });
 
     child.stdin.write(pageContent);
